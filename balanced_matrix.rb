0448f8ff6b54e9ccb3f9ed58d73de6bc7433cc5f
Michael Cheng <michael_cheng@tech.gov.sg>
2021-03-04 21:37:18 +0800

    Display a grid.

--- balanced_matrix.rb
+++ balanced_matrix.rb
@@ -1,3 +1,17 @@
 class BalancedMatrix
+  attr_reader :grid
 
+  def initialize(size=0, initial=[])
+    @size = size
+    @data = initial
+    @grid = build_grid
+  end
+
+  private
+
+  def build_grid
+    @size.times.map do |row|
+      @size.times.map { |col| @data[(row * @size) + col] }
+    end
+  end
 end
--- spec/balanced_matrix_spec.rb
+++ spec/balanced_matrix_spec.rb
@@ -5,4 +5,29 @@ describe BalancedMatrix do
   it 'builds a matrix' do
     expect(subject).to be_a(BalancedMatrix)
   end
+
+  describe '#grid' do
+    it 'shows 2D array' do
+      expect(subject.grid).to eq([])
+    end
+
+    describe '2x2 grid' do
+      subject{ BalancedMatrix.new(2) }
+
+      it 'shows 2D array' do
+        expect(subject.grid).to eq([[nil, nil], [nil, nil]])
+      end
+    end
+
+    describe 'populated grid' do
+      let(:size) { 2 }
+      let(:initial) { [1,0,1,0] }
+
+      subject{ BalancedMatrix.new(size, initial) }
+
+      it 'shows 2D array' do
+        expect(subject.grid).to eq([[1, 0], [1, 0]])
+      end
+    end
+  end
 end
